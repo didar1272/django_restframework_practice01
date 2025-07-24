7efc1e1035c50cf55e997ae785109c3d9e392a94
Md. Didarul Islam <didar@Mds-MacBook-Pro.local>
2025-07-25 00:14:57 +0600

    generic views added

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
 from django.http import JsonResponse
+from products.models import Product
+from django.forms.models import model_to_dict
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from products.serializers import ProductSerializer
+import json
 
 
+@api_view(["POST"])
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "Hello, World!"})
+    "DRF API"
+    # instance = Product.objects.all().order_by("?").first()
+    # data = {}
+    # if instance:
+    #     # data = model_to_dict(model_data, fields=['id', 'title', 'content', 'sale_price'])
+    #     data = ProductSerializer(instance).data
+
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        data = serializer.data
+        print(data)
+        return Response(data)
